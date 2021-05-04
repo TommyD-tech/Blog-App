11771bb137df64db8982296d578a4eeb19c4973f
Olaide Folowososele <olaideayotomiwa@gmail.com>
2021-05-04 13:01:42 +0100

    Forms and user accounts

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,9 @@
+
+from django.conf import settings
+from django.contrib.auth import get_user_model
 from django.db import models
+from django.urls import reverse
+
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
@@ -9,4 +14,25 @@ class Post(models.Model):
     body = models.TextField()
 
     def __str__(self):
-        return self.title
+        return self.title[:15]
+
+    def get_absolute_url(self):
+        return reverse('post_detail', args=[str(self.id)])
+
+class Comment(models.Model): # new
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments') 
+    comment = models.CharField(max_length=140, null=True, blank=True, default='DEFAULT VALUE') 
+    author = models.ForeignKey( 
+        get_user_model(), 
+        on_delete=models.CASCADE, 
+        related_name='comments',
+    )
+
+
+    def __str__(self): 
+        return self.comment
+
+    def get_absolute_url(self):
+        return reverse('post_detail')
+
+    
--- config/settings.py
+++ config/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
+    'accounts',
+    'pages',
 ]
 
 MIDDLEWARE = [
@@ -125,3 +127,5 @@ STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))] # new
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
