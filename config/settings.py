9307cb08d5dc896e5c0fa18796075c22d8b6713d
Olaide Folowososele <olaideayotomiwa@gmail.com>
2021-05-04 21:18:21 +0100

    Inotial commit

--- config/settings.py
+++ config/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = str(os.getenv('DEBUG'))
 
-ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['ola-blogapp.herokuapp.com']
 
 
 # Application definition
